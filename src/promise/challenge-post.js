d031881a582d08388ea6b61f36cdcc7bd1a9c2d5
name <williangel10@outlook.com>
2024-01-10 14:12:13 -0400

    agregamos el aporte de alguien para hacer las operaciones: DELETE Y UPDATE

--- src/promise/challenge-post.js
+++ src/promise/challenge-post.js
@@ -25,3 +25,33 @@ const data = {
 postData(`${API}/products`, data)
   .then(response => response.json())
   .then(data => console.log(data))
+
+
+//aportes de otros compa;eros
+
+//Update (solo cambié 'POST' por 'PUT'):
+
+
+const updateData = (urlApi, data) => {
+    const response = fetch(urlApi, {
+        method: 'PUT',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    });
+    return response;
+};
+//Y para Delete quité headers y body puesto que lo unico que mandamos es el ID en la url para eliminar el producto de acuerdo con la documentación.
+
+
+const deleteData = (urlApi) => {
+    const response = fetch(urlApi, {
+        method: 'DELETE',
+        mode: 'cors',
+        credentials: 'same-origin',
+    });
+    return response;
+}
